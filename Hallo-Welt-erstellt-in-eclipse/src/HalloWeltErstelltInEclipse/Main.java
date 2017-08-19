b7ca4018564a33bc00842b9e37d8c15de8618ba6
tester <tester@DESKTOP-058GEG5>
2017-08-19 15:36:59 +0200

    erster lokaler commit(kopieren in den von git verwalteten Bereich)

---
+++ Hallo-Welt-erstellt-in-eclipse/src/HalloWeltErstelltInEclipse/Main.java
@@ -0,0 +1,10 @@
+package HalloWeltErstelltInEclipse;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Halle Welt, erstellt mit Java in Eclipse");
+
+	}
+
+}
